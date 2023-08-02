ff574c8587d144d1c1ba06736a41ee09ea95107b
Sabarinath P <sabarinathp@Sabarinaths-MacBook-Air.local>
2023-08-02 11:37:43 +0530

    signup page corrected

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_mobile/screens/register.dart';
 import 'package:e_commerce_mobile/styles.dart/sizer.dart';
 import 'package:e_commerce_mobile/styles.dart/textstyle.dart';
 import 'package:flutter/material.dart';
@@ -12,79 +13,82 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body:Container(
-      margin: EdgeInsets.all(20),
-      child: Column(
-       
-        children: [
-          Align(alignment: Alignment.centerLeft,
-        child: Padding(
-          padding: EdgeInsets.only(left: 1.0),
-          child:tx700("Login"),),),
-         
-         height(15), 
+          body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 1.0),
+                child: tx700("Login"),
+              ),
+            ),
+            height(15),
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 1.0),
+                    child: tx400(
+                        "Enter the email address you'd like to use to sign in to HandmadeHive"),
+                  ),
+                ),
+                height(50),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: ("Email/Phone number"),
+                      border: OutlineInputBorder()),
+                ),
+                height(10),
                 Column(
                   children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                    child:Padding(
-                      padding: EdgeInsets.only(left: 1.0),
-                     child:tx400("Enter the email address you'd like to use to sign in to HandmadeHive"),),
-                 ),
-                    height (50),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: ("Email/Phone number"),
-                      border: OutlineInputBorder()
-                    ),
-                  ),
-                  height(10),
-                  Column(
-                    children: [
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
                           labelText: ("Password"),
-                          border: OutlineInputBorder()
-                        ),
-                      ),
-                      Align(
+                          border: OutlineInputBorder()),
+                    ),
+                    Align(
                         alignment: Alignment.centerRight,
-                        child: Padding(padding: EdgeInsets.only(right: 1.0),
-                        child: tx500("Forgot password?"),)
-                      ),height(15),
-                      Container(
-                        height: 45,
-                        alignment: Alignment.center,
-                        
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),color: Colors.redAccent),
-                        child:tx400("Login"),
-                      ),
-                     height (10),
-                     
-                     Row(
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 1.0),
+                          child: tx500("Forgot password?"),
+                        )),
+                    height(15),
+                    Container(
+                      height: 45,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.redAccent),
+                      child: tx400("Login"),
+                    ),
+                    height(10),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
-                       children: [
-                         tx500("Don't have an account?"),
-                         tx500("Sign up")
-                       ],
-                     )
-                    ],
-                  )
-                  
+                      children: [
+                        tx500("Don't have an account?"),
+                        InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => SignupScreen()));
+                            },
+                            child: tx500("Sign up"))
+                      ],
+                    )
                   ],
-                ),
-              
-              
-        ],
-      ),
-      
-    )
-      ),
+                )
+              ],
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
