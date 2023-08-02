1ced2d75cfea32993e74cf405cded83d042d0c43
unknown <nandanasanal321@gmail.com>
2023-08-02 11:25:53 +0530

    funda push

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -13,7 +13,104 @@ class _SignupScreenState extends State<SignupScreen> {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: [],
+          children: [
+        SizedBox(height:50),
+         Align(
+                      alignment: Alignment.centerLeft,
+                    child:Padding(
+                      padding: EdgeInsets.only(left: 6),
+                     child:Text("Welcome to HandmadeHive!",style: TextStyle(fontSize: 20,color: Colors.black),)),
+                 ),
+                 SizedBox(height: 30,),
+                Container( margin: EdgeInsets.only(left: 5),
+                  child:TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 5),
+                      labelText: ("Full Name"),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide(width:4),
+                      )
+                    ),
+                  ),),
+                   SizedBox(height: 10,),
+                 Container( margin: EdgeInsets.only(left: 5),
+                  child:TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 3),
+                      labelText: ("Email"),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide(width:4),
+                      )
+                    ),
+                  ),),
+                   SizedBox(height: 10,),
+                 Container(margin: EdgeInsets.only(left: 5),
+                 child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 3),
+                      labelText: ("Phone Number"),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide(width:4),
+                      )
+                    ),),
+                  ),
+                    SizedBox(height: 10,),
+                Container(margin:EdgeInsets.only(left: 5),
+                child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                     
+                      contentPadding: EdgeInsets.only(left: 3),
+                      labelText: ("Password"),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide(width:4),
+                      )
+                    
+                    ),),
+                  ),
+                  SizedBox(height: 10,),
+                 Container(margin: EdgeInsets.only(left: 5),
+                 child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 3),
+                      labelText: ("Confirm Password"),
+                      border: OutlineInputBorder(
+                       borderSide: BorderSide(width:4),
+                      )
+                    
+                    ),),
+                  ),
+                  SizedBox(height: 15,),
+                  Column(
+                    children: [
+                      Container(
+                        width:MediaQuery.of(context).size.width,
+                        height: 45,
+                        color: Colors.redAccent,
+                        alignment: Alignment.center,
+                        child: Text("Sign up",style: TextStyle(fontSize: 15,color: Colors.white),),
+                      ),
+                    
+                      Container( 
+                        alignment: Alignment.center,
+                        child:
+                            Text("By signing up you agree to our Terms and Conditions and Privacy Policy",style: TextStyle(fontSize: 15,color:Colors.black),),
+                          ),
+                          Expanded(child: Container()),
+                     Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Text("Already have an account? Login",style: TextStyle(fontSize: 15,color: Colors.black),),
+                     )                   
+                  
+                        
+                      
+                    ],
+                  )
+],
         ),
       ),
     );
